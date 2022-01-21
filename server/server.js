d476f550a032909c93fd457e1460702fb0468882
duong_dinh <duong.dinh@marketingclinic.fi>
2022-01-22 00:53:55 +0200

    fix conflicts

--- server/server.js
+++ server/server.js
@@ -26,11 +26,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-<<<<<<< HEAD
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-=======
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
->>>>>>> 1a3f819 (re-commit)
   );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
