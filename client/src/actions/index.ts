9f3c8b941178702dbeb4e4b30e904f73843cee28
duong_dinh <duong.dinh@marketingclinic.fi>
2023-02-15 13:14:26 +0200

    remove unused files

--- client/src/actions/index.ts
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-import * as notificationActions from "./notification";
-import * as itineraryActions from "./itineraries";
-import * as searchResultActions from "./searchResult";
-
-export { notificationActions, itineraryActions, searchResultActions };
