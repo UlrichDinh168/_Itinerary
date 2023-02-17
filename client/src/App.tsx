6a185f1a52bd2703af030d5050cc92fe80886205
Ulrich Dinh <duong0028@gmail.com>
2023-02-17 21:00:09 +0200

    remove helmet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,10 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { Container } from "./components";
 import Snackbar from "./shared/SnackBar";
-import Helmet from "react-helmet";
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <Helmet titleTemplate="Itinerary" defaultTitle="Itinerary">
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Helmet>
       <Container />
       <Snackbar />
     </Provider>
