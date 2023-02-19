f2c11668b8ff8fbd5624bccb5f42b51712a64574
Ulrich Dinh <duong0028@gmail.com>
2023-02-19 22:57:58 +0200

    change store dir

--- client/src/reducers/searchResult.ts
+++ client/src/reducers/searchResult.ts
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import axios from "axios";
-import { AppThunk, AppDispatch } from "store";
+import { AppThunk, AppDispatch } from "../store";
 const instance = axios.create({
   baseURL: "http://localhost:8000",
 });
